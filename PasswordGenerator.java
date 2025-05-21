4e76d0423d99568a54e65c03f7e3dad6b7ffc1a0
raghu683 <ankalaraghu04@gmail.com>
2025-05-21 07:35:30 +0530

    Add files via upload

---
+++ PasswordGenerator.java
@@ -0,0 +1,61 @@
+import java.security.SecureRandom;
+import java.util.Scanner;
+
+public class PasswordGenerator {
+
+    private static final String UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
+    private static final String DIGITS = "0123456789";
+    private static final String SPECIAL_CHARS = "!@#$%^&*()-_+=<>?/{}~";
+    private static final String ALL_CHARS = UPPERCASE + LOWERCASE + DIGITS + SPECIAL_CHARS;
+
+    private static final SecureRandom random = new SecureRandom();
+
+    public static String generatePassword(int length) {
+        if (length < 4) {
+            throw new IllegalArgumentException("Password length should be at least 4 characters.");
+        }
+
+        StringBuilder password = new StringBuilder(length);
+
+        // Ensure password contains at least one character from each category
+        password.append(UPPERCASE.charAt(random.nextInt(UPPERCASE.length())));
+        password.append(LOWERCASE.charAt(random.nextInt(LOWERCASE.length())));
+        password.append(DIGITS.charAt(random.nextInt(DIGITS.length())));
+        password.append(SPECIAL_CHARS.charAt(random.nextInt(SPECIAL_CHARS.length())));
+
+        // Fill remaining characters
+        for (int i = 4; i < length; i++) {
+            password.append(ALL_CHARS.charAt(random.nextInt(ALL_CHARS.length())));
+        }
+
+        // Shuffle the characters for randomness
+        return shuffleString(password.toString());
+    }
+
+    private static String shuffleString(String input) {
+        char[] a = input.toCharArray();
+        for (int i = a.length - 1; i > 0; i--) {
+            int j = random.nextInt(i + 1);
+            char temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+        return new String(a);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter desired password length: ");
+        int length = scanner.nextInt();
+
+        try {
+            String password = generatePassword(length);
+            System.out.println("Generated Password: " + password);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        scanner.close();
+    }
+}
